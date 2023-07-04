3d304b5a825cceca2573d6a42709a426ef8c33a6
Rob Thomson <rob@marotori.com>
2023-07-04 10:30:33 +0100

    Using new rpm measurement

--- s.portGasHub.ino
+++ s.portGasHub.ino
@@ -3,6 +3,10 @@
 // https://github.com/RealTadango/FrSky/blob/master/examples/SimpleSensor/SimpleSensor.ino
 // you will need to add his libraries to your arduoino environment for it to work.
 //
+// FreqMeasure can be found here: https://www.pjrc.com/teensy/td_libs_FreqMeasure.html
+//
+// Timer https://github.com/contrem/arduino-timer
+//
 // Measuring voltage will need voltage dividers - you cannot send more than 3v to the arduino
 // A pins.   Suggested divider ration is 10k / 2k.  This will be sufficient for a 2s lipo
 // to be used for the pack and ignition system.
@@ -12,11 +16,13 @@
 // a 100pf capacitor can also be of benifit linked from the cdi pin to gnd.
 
 #include <SPort.h>                  //Include the SPort library
+#include <FreqMeasure.h>
+#include <arduino-timer.h>
 
 #define SPORT_PIN 9         //a digital pin to send s.port data to frsky receiver.
 #define VOLTAGE_PIN1 A2     //analog pin used to read a voltage from receiver battery
 #define VOLTAGE_PIN2 A3     //analog pin used to read a voltage from the ignition battery
-#define RPM_PIN 5           //digital pin 5 must be used for CDI timing.  Do not change
+#define RPM_PIN 8           //digital pin 8 must be used for CDI timing.  Do not change
 
 
 #define SENSOR_ID1 0x5900   //unique id number for voltage read on VOLTAGE_PIN1
@@ -33,25 +39,13 @@ SimpleSPortSensor sensor3(SENSOR_ID3);
 SimpleSPortSensor sensor4(SENSOR_ID4);  
 SimpleSPortSensor sensor5(SENSOR_ID5);  
 
-//rpmbits
-volatile unsigned long totalCounts;
-volatile bool nextCount;
-volatile unsigned long Timer1overflowCounts;
-volatile unsigned long overflowCounts;
-unsigned int counter, countPeriod;
-
 // variables used by kalman filter to smooth throttle readings.
 double kalman_q= 0.05;   
 double kalman_r= 150;   
 
 //RPM
-#define WINDOW_SIZE 5  
-int INDEX = 0;
-int VALUE = 0;
-int SUM = 0;
-int READINGS[WINDOW_SIZE];
-int AVERAGED = 0;
-
+unsigned long rpmHZ = 0;
+int lastRPMms = 0;
 
 // voltage divider ration - this is used as a multiplier to go from the 3v signal to the real received signal
 float dividerRatio = 6.065;
@@ -63,7 +57,9 @@ float sensorValue3;
 float sensorValue4;
 float sensorValue5;
 
-
+//timer
+auto timer = timer_create_default();
+  
 
 void setup() {
   hub.registerSensor(sensor1);       //Add sensor to the hub
@@ -72,139 +68,71 @@ void setup() {
   hub.registerSensor(sensor4);       //Add sensor to the hub
   hub.registerSensor(sensor5);       //Add sensor to the hub
   
-  pinMode(RPM_PIN, INPUT);           // enable rpm reading on pin
-
-  pinMode(12,OUTPUT);
+  //pinMode(12,OUTPUT);
 
   hub.begin();                      //start the s.port transmition
-  
- // Serial.begin(115200); // enable serial port if code debugging.
+
+  FreqMeasure.begin();
+
+  timer.every(5000, getVoltages);  //update voltage every 5 seconds
+
+  Serial.begin(115200); // enable serial port if code debugging.
 
 }
 
+double sum=0;
+int count=0;
 void loop() {
 
 
-
-          startCount(1000);
-          while(!nextCount) {
-                hub.handle(); //keep s.port data current.
-          }
-        
-            //GET VOLTAGE1
-            sensorValue1 =  kalman_update1(analogRead(VOLTAGE_PIN1));
-            float voltage1 = sensorValue1 * (5.0 / 1023.0);
-            sensor1.value = (voltage1 * dividerRatio) * 100 ;      
+ 
+            //SET VOLTAGE1
+            sensor1.value = sensorValue1;   
       
-            //GET VOLTAGE2
-            sensorValue2 =  kalman_update2(analogRead(VOLTAGE_PIN2));
-            //sensorValue2 =  analogRead(VOLTAGE_PIN2);
-            float voltage2 = sensorValue2 * (5.0 / 1023.0);
-            sensor2.value = (voltage2 * dividerRatio) * 100 ;     
-   
-           
-            //RPM
-            SUM = SUM - READINGS[INDEX];       // Remove the oldest entry from the sum
-            VALUE = totalCounts;        // Read the next sensor value
-            READINGS[INDEX] = VALUE;           // Add the newest reading to the window
-            SUM = SUM + VALUE;                 // Add the newest reading to the sum
-            INDEX = (INDEX+1) % WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
-          
-            AVERAGED = SUM / WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
-              
-            sensorValue3 = (AVERAGED * 60);
-            sensor3.value = sensorValue3; 
-            //HZ
-            sensorValue4 = AVERAGED;
-            sensor4.value = AVERAGED;     
+            //SET VOLTAGE2
+            sensor2.value = sensorValue2; 
 
-            //HZ
-            if(sensorValue3 <= 60){
+
+            if (FreqMeasure.available()) {
+                // average several reading together
+                sum = sum + FreqMeasure.read();
+                count = count + 1;
+                if (count > 30) {
+                  rpmHZ = FreqMeasure.countToFrequency(sum / count);
+                  sum = 0;
+                  count = 0;
+                  lastRPMms = millis();
+                }
+              } 
+
+            //time out the measurements if nothing for some time.
+            if((millis() - lastRPMms) > 2000){
+                rpmHZ = 0;         
+            }
+           
+            //CDI RPM VALUE
+            sensorValue3 = (rpmHZ * 60);
+            sensor3.value = sensorValue3; 
+            
+            //CDI HZ VALUE
+            sensorValue4 = rpmHZ;
+            sensor4.value =  sensorValue4;     
+
+            //ENGINE RUNNING/NOT RUNNING
+            if(rpmHZ <= 10){
                  sensorValue5 = 0;
             } else {
                 sensorValue5 = 1;
             }
             sensor5.value = sensorValue5;     
 
-        
 
+            hub.handle();    
+            timer.tick();
 }
 
-// kalman filter to 'denoise' the measurements.
-float kalman_update1(float measurement)
-{
-  //static int lcnt=0;
-  static float x=sensorValue1; //value
-  static float p=100; //estimation error covariance
-  static float k=0; //kalman gain
-
-  // update the prediction value
-  p = p + kalman_q;
-
-  // update based on measurement
-  k = p / (p + kalman_r);
-  x = x + k * (measurement - x);
-  p = (1 - k) * p;
-
-  return x;
-}
-
-//kalman filter to 'denoise' the measurements.
-float kalman_update2(float measurement)
-{
-  //static int lcnt=0;
-  static float x=sensorValue2; //value
-  static float p=100; //estimation error covariance
-  static float k=0; //kalman gain
-
-  // update the prediction value
-  p = p + kalman_q;
-
-  // update based on measurement
-  k = p / (p + kalman_r);
-  x = x + k * (measurement - x);
-  p = (1 - k) * p;
-
-  return x;
-}
-
-void startCount(unsigned int period)
-{
-  nextCount = false;
-  counter = 0;
-  Timer1overflowCounts = 0;
-  countPeriod = period;
-  
-  //Timer 1: overflow interrupt due to rising edge pulses on D5
-  //Timer 2: compare match interrupt every 1ms
-  noInterrupts();
-  TCCR1A = 0; TCCR1B = 0; //Timer 1 reset
-  TCCR2A = 0; TCCR2B = 0; //Timer 2 reset
-  TIMSK1 |= 0b00000001;   //Timer 1 overflow interrupt enable
-  TCCR2A |= 0b00000010;   //Timer 2 set to CTC mode
-  OCR2A = 124;            //Timer 2 count upto 125
-  TIMSK2 |= 0b00000010;   //Timer 2 compare match interrupt enable
-  TCNT1 = 0; TCNT2 = 0;   //Timer 1 & 2 counters set to zero
-  TCCR2B |= 0b00000101;   //Timer 2 prescaler set to 128
-  TCCR1B |= 0b00000111;   //Timer 1 external clk source on pin D5
-  interrupts();
-}
-//=================================================================
-ISR(TIMER1_OVF_vect)
-{
-  Timer1overflowCounts++;
-}
-//=================================================================
-ISR (TIMER2_COMPA_vect)
-{
-  overflowCounts = Timer1overflowCounts;
-  counter++;
-  if(counter < countPeriod) return;
-
-  TCCR1A = 0; TCCR1B = 0;   //Timer 1 reset  
-  TCCR2A = 0; TCCR2B = 0;   //Timer 2 reset    
-  TIMSK1 = 0; TIMSK2 = 0;   //Timer 1 & 2 disable interrupts
-
-  totalCounts = (overflowCounts * 65536) + TCNT1;
-  nextCount = true;
+bool getVoltages(){
+            sensorValue1= ((analogRead(VOLTAGE_PIN1) * (5.0 / 1023.0)) *  dividerRatio) * 100;
+            sensorValue2 = ((analogRead(VOLTAGE_PIN2) * (5.0 / 1023.0)) * dividerRatio) * 100;            
+            return true; 
 }
